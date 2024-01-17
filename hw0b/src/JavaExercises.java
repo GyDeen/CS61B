929bd4fad7f1b983912b44b5b7e2ea43413edf5b
Ergun Acikoz <ergunackz@gmail.com>
2024-01-17 22:35:46 +0300

    Updating hw0b skeleton

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,14 +1,12 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 
 public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        int[] array = {1,2,3,4,5,6};
-        return array;
+        // TODO: Fill in this function.
+        return null;
     }
 
     /** Returns the order depending on the customer.
@@ -16,24 +14,15 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        if (customer.equals("Ergun")){
-            String[] array = {"beyti", "pizza", "hamburger", "tea"};
-            return array;
-        } else if (customer.equals("Erik")) {
-            String[] array = {"sushi", "pasta", "avocado", "coffee"};
-            return array;
-        }
-        String[] array = new String[3];
-        return array;
+        // TODO: Fill in this function.
+        return null;
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        int distance = 0, length = array.length;
-        Arrays.sort(array);
-        distance = array[length - 1] - array[0];
-        return distance;
+        // TODO: Fill in this function.
+        return 0;
     }
 
     /**
@@ -49,14 +38,8 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        list.add(x);
-        if (x == 1) {
-            return list;
-        }
-        if (x % 2 != 0) {
-            return hailstoneHelper(x*3+1, list);
-        }
-        return hailstoneHelper(x/2, list);
+        // TODO: Fill in this function.
+        return null;
     }
 
 }
--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,55 +1,29 @@
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
 	public static int sum(List<Integer> L) {
-        int length = L.size(), total = 0;
-        for (Integer integer : L) {
-            total += integer;
-        }
-        return total;
+        // TODO: Fill in this function.
+        return 0;
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        List<Integer> even_list = new ArrayList<>();
-        for (int num : L){
-            if (num % 2 == 0) {
-                even_list.add(num);
-            }
-        }
-        return even_list;
+        // TODO: Fill in this function.
+        return null;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        Set<Integer> set2 = new HashSet<>(L2);
-        Set<Integer> common_set = new HashSet<>();
-        for (int num : L1) {
-            if (set2.contains(num)) {
-                common_set.add(num);
-            }
-        }
-                return new ArrayList<>(common_set);
+        // TODO: Fill in this function.
+        return null;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
         // TODO: Fill in this function.
-        int times = 0;
-        for (String str : words){
-            char[] chars = str.toCharArray();
-            for (char ch : chars){
-                if (ch == c){
-                    times++;
-                }
-            }
-        }
-        return times;
+        return 0;
     }
 }
