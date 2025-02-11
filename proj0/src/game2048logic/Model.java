b12673ab93cc1c51c0905c368f29f0166c25fea9
Gydeen <deenz@DeenZdeMacBook-Air.local>
2025-02-12 00:07:21 +0800

    Partialy finished moveTileUpAsFarAsPossible

--- proj0/src/game2048logic/Model.java
+++ proj0/src/game2048logic/Model.java
@@ -140,30 +140,39 @@ public class Model {
             return true;
         }
 
-        int x = 0, y = 0, board_size = size();
-        for (;x < board_size; x++) {
-            for (; y < board_size; y++) {
+        int board_size = size();
+        for (int x = 0;x < board_size; x++) {
+            for (int y =0; y < board_size; y++) {
                 Tile current_tile = tile(x, y);
-                if (y == board_size - 1) {
-                    y = 0;
-                }
 
-                if (x < board_size - 1) {
-                    // if the tile is not the top-most tile
+                // if current tile is right-most tile
+                if (x == board_size - 1) {
+                    // if it is the top-right tile
+                    if (y == board_size - 1) {
+                        continue;
+                    }
+                    Tile tile0 = tile(x, y + 1); // only need to check above tile
+                    if (can_move(current_tile, tile0)) {
+                        return true;
+                    }
+                }
+                else {
+                    // if current tile is not the top-most tile and not right-most tile
                     if (y < board_size - 1) {
-                        Tile tile0 = tile(x, y + 1), tile1 = tile(x + 1, y);
+                        Tile tile0 = tile(x, y + 1), tile1 = tile(x + 1, y); // need to check both above tile and RHS tile
                         if (can_move(current_tile, tile0) || can_move(current_tile, tile1)) {
                             return true;
                         }
                     }
-                    // if the tile is the top-most tile
+                    // if current tile is the top-most tile and not right-most tile
                     else {
-                        Tile tile1 = tile(x + 1, y);
+                        Tile tile1 = tile(x + 1, y); // only need to check the RHS tile
                         if (can_move(current_tile, tile1)) {
                             return true;
                         }
                     }
                 }
+
             }
         }
 
@@ -187,8 +196,31 @@ public class Model {
     public void moveTileUpAsFarAsPossible(int x, int y) {
         Tile currTile = board.tile(x, y);
         int myValue = currTile.value();
-        int targetY = y;
+        int targetY = y, board_size = board.size();
 
+        // move upper until there is tile or reach the edge
+        while (targetY < board_size - 1) {
+            // upper tile is not empty
+            if (!is_empty(x , targetY + 1)) {
+                Tile upper_tile = board.tile(x, targetY + 1);
+                int upper_value = upper_tile.value();
+                // if they have the same value, move and merge those two tile
+                if (myValue == upper_value) {
+                    targetY += 1;
+                    board.move(x , targetY, currTile);
+                    return;
+                } else {
+                    break;
+                }
+            }
+            // upper tile is empty, looking for next tile
+            else {
+                targetY += 1;
+            }
+        }
+;
+        // got the target Y we can move the tile now
+        board.move(x, targetY, currTile);
         // TODO: Tasks 5, 6, and 10. Fill in this function.
     }
 
