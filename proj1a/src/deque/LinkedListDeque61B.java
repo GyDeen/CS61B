ca30427f9136370b40114754dba4c3a39f9d25d5
Gydeen <zde421217757@outlook.com>
2025-02-21 17:25:15 +0800

    finished addFirst and toList

--- proj1a/src/deque/LinkedListDeque61B.java
+++ proj1a/src/deque/LinkedListDeque61B.java
@@ -1,8 +1,9 @@
 package deque;
 
+import java.util.LinkedList;
 import java.util.List;
 
-public class LinkedListDeque61B<T> implements Deque61B<T>{
+public class LinkedListDeque61B<T> implements Deque61B<T> {
 
     private class Node {
         T item;
@@ -10,65 +11,86 @@ public class LinkedListDeque61B<T> implements Deque61B<T>{
         Node next;
 
         // Constructor for Node class
-        public Node(T item) {
+        public Node(T item, Node n) {
             this.item = item;
-            this.prev = null;
+            this.prev = n;
             this.next = null;
         }
     }
 
-    private Node sentinal;
+    private final Node sentinel;
     private int size;
 
     public LinkedListDeque61B() {
-        sentinal = new Node(null);
-        sentinal.next = sentinal;
-        sentinal.prev = sentinal;
+        sentinel = new Node(null, null);
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
         size = 0;
     }
 
+
     @Override
     public void addFirst(T x) {
+        Node new_node = new Node(x, sentinel);
+        Node current_node = sentinel.next;
 
+        sentinel.next = new_node;
+        new_node.next = current_node;
+        current_node.prev = new_node;
+
+        this.size++;
     }
 
     @Override
     public void addLast(T x) {
-
+        // Add last logic here
     }
 
     @Override
     public List<T> toList() {
-        return List.of();
+        List<T> new_list = new LinkedList<>();
+
+        Node current = sentinel.next;
+        while (current != sentinel) {
+            new_list.add(current.item);
+            current = current.next;
+        }
+
+        return new_list;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public T removeFirst() {
+        // Remove first logic here
         return null;
     }
 
     @Override
     public T removeLast() {
+        // Remove last logic here
         return null;
     }
 
     @Override
     public T get(int index) {
+
         return null;
     }
 
     @Override
     public T getRecursive(int index) {
+        // Recursive get logic here
         return null;
     }
 }
+
