9f011daeea2d5976573b0296e4b6c97b1695d881
Gydeen <zde421217757@outlook.com>
2025-03-05 17:39:43 +1100

    Working on ArrayDeque61B.java

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -1,13 +1,38 @@
 import java.util.List;
 import java.lang.Math;
 
+
 public class ArrayDeque61B<T> implements Deque61B<T> {
+    private T[] items;
+    private int size;
+    private int front;
+    private int back;
+
+    private static final int INITIAL_CAPACITY = 8;
+    private static final int INITIAL_FRONT = 4;
+    private static final int INITIAL_BACK = 5;
+
     public ArrayDeque61B() {
+        items = (T[]) new Object[INITIAL_CAPACITY];
+        size = 0;
+        front = INITIAL_FRONT;
+        back = INITIAL_BACK;
     }
+
+    // there is no enough space for new item, double current array
+    public void resize(int size) {
+
+    }
+
 
     @Override
     public void addFirst(T x) {
-
+        if (size == items.length) {
+            resize(size * 2);
+        }
+        front = Math.floorMod(front - 1, items.length);
+        items[front] = x;
+        size++;
     }
 
     @Override
