83a2f90ad4a3eb637936dfce1119e8c0141ddda1
Gydeen <zde421217757@outlook.com>
2025-03-11 20:17:57 +1100

    Implementing test

--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -26,13 +26,13 @@ public class ArrayDeque61BTest {
         Deque61B<String> lld1 = new ArrayDeque61B<>();
 
         lld1.addFirst("back"); // after this call we expect: ["back"]
-        assertThat(lld1.toList()).containsExactly("back").inOrder();
+        assertThat(lld1.toList()).containsExactly(null,null,null,null,"back",null,null,null).inOrder();
 
         lld1.addFirst("middle"); // after this call we expect: ["middle", "back"]
-        assertThat(lld1.toList()).containsExactly("middle", "back").inOrder();
+        assertThat(lld1.toList()).containsExactly(null,null,null,"middle", "back",null,null,null).inOrder();
 
         lld1.addFirst("front"); // after this call we expect: ["front", "middle", "back"]
-        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+        assertThat(lld1.toList()).containsExactly(null,null,"front", "middle", "back",null,null,null).inOrder();
 
          /* Note: The first two assertThat statements aren't really necessary. For example, it's hard
             to imagine a bug in your code that would lead to ["front"] and ["front", "middle"] failing,
@@ -47,7 +47,7 @@ public class ArrayDeque61BTest {
         lld1.addLast("front"); // after this call we expect: ["front"]
         lld1.addLast("middle"); // after this call we expect: ["front", "middle"]
         lld1.addLast("back"); // after this call we expect: ["front", "middle", "back"]
-        assertThat(lld1.toList()).containsExactly("front", "middle", "back").inOrder();
+        assertThat(lld1.toList()).containsExactly(null,null,null,null,null,"front","middle","back").inOrder();
     }
 
     @Test
