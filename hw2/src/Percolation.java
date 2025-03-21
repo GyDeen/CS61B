4b9a949af69c6039e69321405297c71999cdfe2d
Gydeen <zde421217757@outlook.com>
2025-03-21 14:24:45 +1100

    Removed class Node

--- hw2/src/Percolation.java
+++ hw2/src/Percolation.java
@@ -2,32 +2,23 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 
 public class Percolation {
-    private int boardSize;
+    private final int IS_FULL = 1;
+    private final int IS_BLOCK = -1;
+    private final int IS_EMPTY = 0;
+    private final int IS_OPEN = 2;
+    private final int NOT_OPEN = -2;
+
+
+    private final int boardSize;
     private boolean isPercolation;
     private WeightedQuickUnionUF board;
 
-
-    private class Node {
-        private final int IS_FULL = 1;
-        private final int IS_BLOCK = -1;
-        private final int IS_EMPTY = 0;
-        private final int IS_OPEN = 2;
-        private final int NOT_OPEN = -2;
-
-        private boolean isTop = false;
-        private int statement = NOT_OPEN; // Statement for whether is full, block or empty
-        private int x;
-        private int y;
+    private boolean isTop = false;
+    private int statement = NOT_OPEN; // Statement for whether is full, block or empty
+    private int x;
+    private int y;
 
 
-        public Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-
-            if (x == 0) {
-                isTop = true;
-            }
-        }
 
         public void setStatement(int i) {
             statement = i;
@@ -35,6 +26,10 @@ public class Percolation {
 
         public int getStatement() {
             return statement;
+        }
+
+        public int getBoardIdx() {
+
         }
     }
     public Percolation(int N) {
