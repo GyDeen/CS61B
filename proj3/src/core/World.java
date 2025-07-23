b9011e2b03f0126c1b7d895f7e146219deaa66b0
GyDeen <zde421217757@outlook.com>
2025-07-23 16:44:20 +1000

    Starting on proj 3

--- proj3/src/core/World.java
+++ proj3/src/core/World.java
@@ -1,7 +1,29 @@
 package core;
 
+import edu.princeton.cs.algs4.StdDraw;
+import tileengine.TERenderer;
+import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
 public class World {
+    private static final int HEIGHT = 200;
+    private static final int WIDTH = 200;
+    private static final long seed = 726;
+    private static final Random RANDOM = new Random(seed);
 
-    // build your own world!
+    private static void reedemWorld() {
 
+    }
 }
+
+
+
