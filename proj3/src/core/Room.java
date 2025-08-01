42628648ba3a671ab045290cf77801264cdc3179
GyDeen <zde421217757@outlook.com>
2025-08-01 15:25:01 +1000

    Method for checking validation of newly generated room

--- proj3/src/core/Room.java
+++ proj3/src/core/Room.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Room extends TerrainInfo{
     private TileType floorType;
@@ -28,6 +29,39 @@ public class Room extends TerrainInfo{
     /** Draw a room with given TileType, the width = inside room + CANT_PASS block numbers.
      */
     public void drawRoomCornerOrNot() {
+
+    }
+
+
+    /** Testing whether input room is a valid room. It couldn't have overlap with other rooms. It cannot make the floor
+     * adjacent with the edge of the world, that is, the "floor" of the room need to be surrounded by "wall"
+     * @param room the room that need validation
+     * @param rooms the room that already exist */
+    public static boolean validRoom(Room room, ArrayList<Room> rooms) {}
+
+
+
+    /** Testing whether input room is a valid room. It couldn't have overlap with other rooms. It cannot make the floor
+     * adjacent with the edge of the world, that is, the "floor" of the room need to be surrounded by "wall"
+     * @param x the room needs validation's x-axis
+     * @param y the room needs validation's y-axis
+     * @param width the room needs validation's width
+     * @param height the room needs validations' height
+     * @param thickness the thickness of the room's wall
+     * @param rooms the room that already exist */
+    public static boolean validRoom(int x, int y, int width, int height, int thickness, ArrayList<Room> rooms) {
+
+    }
+
+
+    /** Testing whether input room is a valid room. It couldn't have overlap with other rooms. It cannot make the floor
+     * adjacent with the edge of the world, that is, the "floor" of the room need to be surrounded by "wall"
+     * @param location the location of the room
+     * @param width the room needs validation's width
+     * @param height the room needs validations' height
+     * @param thickness the thickness of the room's wall
+     * @param rooms the room that already exist */
+    public static boolean validRoom(Point location, int width, int height, int thickness, ArrayList<Room> rooms) {
 
     }
 }
