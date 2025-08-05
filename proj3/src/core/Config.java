d7e7ef1e8489552bfbbaca4bdc2f97325b8e8d73
GyDeen <zde421217757@outlook.com>
2025-08-05 14:49:52 +1000

    Moving all the constant to Config file

---
+++ proj3/src/core/Config.java
@@ -0,0 +1,21 @@
+package core;
+
+public class Config {
+    // Could self define teh ratio of Floor type of room
+    public static final double ROOM_FLOOR_POSS = 0.5;
+    public static final int BLOCK_WIDTH1 = 1;
+    public static final int BLOCK_WIDTH2 = 2;
+    // Could self define the ratio of thickness of wall
+    public static final double WALL_THICKNESS_1_PROBABILITY = 0.9;
+
+    public static final int BUFFER = 2;
+
+    public static final int WINDOW_HEIGHT = 50;
+    public static final int WINDOW_WIDTH = 90;
+    public static final int WORLD_HEIGHT = WINDOW_HEIGHT - UI.BOTTOM_UI - UI.TOP_UI;
+
+    public static final int MIN_ROOM_WIDTH = 13;
+
+    public static final int MIN_MAIN_ROOM_NUM = 3;
+    public static final int MAX_MAIN_ROOM_NUM = 5;
+}
