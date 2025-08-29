82a921a55be099476861de1c37b9da28c08be3ea
GyDeen <zde421217757@outlook.com>
2025-08-29 22:54:07 +1000

    Start on connecting rooms

--- proj3/src/core/HallwayCarver.java
+++ proj3/src/core/HallwayCarver.java
@@ -1,15 +1,18 @@
 package core;
 
 import tileengine.TETile;
+import tileengine.Tileset;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static tileengine.Tileset.FLOOR;
+
 public class HallwayCarver {
     private static TETile[][] world;
-    private static final boolean[][] floor;
-    private static final boolean[][] wall;
+    private static boolean[][] floor;
+    private static boolean[][] wall;
     ArrayList<Point> door;
 
     public HallwayCarver(TETile[][] world) {
@@ -35,4 +38,34 @@ public class HallwayCarver {
             }
         }
     }
+
+    private void setFloor(int x, int y) {
+        world[x][y] = Tileset.FLOOR;
+        floor[x][y] = true;
+        wall[x][y]  = false;
+    }
+
+    private void setWall(int x, int y) {
+        world[x][y] = Tileset.WALL;
+        wall[x][y]  = true;
+        floor[x][y] = false;
+    }
+
+    private boolean inBounds(int x, int y) {
+        return 0 <= x && x < world.length && 0 <= y && y < world[0].length;
+    }
+
+    public TETile[][] getWorld() { return world; }
+
+
+    public boolean connect(Room a, Room b, boolean placeDoors) {
+        return connect(a, null, b, null, placeDoors);
+    }
+
+    public boolean connect(Room a, Point doorA, Room b, Point doorB, boolean placeDoors) {
+        Point drA = (doorA != null) ? doorA : pickDoorOnPerimeter(a, roomCenter(b));
+        Point drb = (doorB != null) ? doorB : pickDoorOnPerimeter(b, roomCenter(a));
+    }
+
+
 }
