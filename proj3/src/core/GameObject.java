28ceadbe36151838d99adb800332c8761b5739f6
GyDeen <zde421217757@outlook.com>
2025-11-28 15:28:53 +1100

    Start on Player etc.

---
+++ proj3/src/core/GameObject.java
@@ -0,0 +1,47 @@
+package core;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    private Point position;
+    private String imagePath;
+    private int imageWidth;
+    private int imageHeight;
+
+    public GameObject(int x, int y, String imagePath, int imageWidth, int imageHeight) {
+        position = new Point(x, y);
+        this.imagePath = imagePath;
+    }
+
+    public GameObject(Point position, String imagePath, int imageWidth, int imageHeight) {
+        this.position = new Point(position);
+        this.imagePath = imagePath;
+    }
+
+
+    public Point getPosition() {
+        return position;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setPosition(int x, int y) {
+        this.position = new Point(x, y);
+    }
+
+    public int getImageWidth() {
+        return imageWidth;
+    }
+
+
+    public int getImageHeight() {
+        return imageHeight;
+    }
+
+
+    public void drawImage() {
+
+    }
+}
