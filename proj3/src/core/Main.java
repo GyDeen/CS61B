9c24d4ba0ac0e1c8e0f084523c68bc0267870a74
GyDeen <zde421217757@outlook.com>
2025-12-01 15:33:14 +1100

    Be able correctly return to init menu

--- proj3/src/core/Main.java
+++ proj3/src/core/Main.java
@@ -1,16 +1,31 @@
 package core;
 
+import edu.princeton.cs.algs4.StdDraw;
+
 public class Main {
     private enum GameState {INIT_MENU, ENTER_SEED, PLAYING, LOADING, QUIT, PAUSE, SAVE}
     private static GameState state = GameState.INIT_MENU;
     private static World world;
-    private static final InitPage init = new InitPage();
+    private static InitPage init;
     private static final SeedScreen seedScreen = new SeedScreen();
     private static final LoadingPage loading = new LoadingPage();
 
 
+    // Consume all the pending input
+    private static void drainInputs() {
+        while (StdDraw.hasNextKeyTyped()) {
+            StdDraw.nextKeyTyped();
+        }
+
+        while (StdDraw.isMousePressed()) {
+            StdDraw.show();
+        }
+    }
+
+
     public static void main(String[] args) {
         while (state != GameState.QUIT) {
+            // Switch state based on Pause Page's result
             if (state == GameState.PAUSE) {
                 PausePage pause = new PausePage();
                 PausePage.PauseChoice pauseChoice = pause.run();
@@ -25,7 +40,10 @@ public class Main {
                     case SAVE_GAME -> state = GameState.SAVE;
                 }
             }
+            // Based on Init Menu result to switch state
             if (state == GameState.INIT_MENU) {
+                drainInputs();
+                init = new InitPage();
                 init.setUpScreen();
                 InitPage.MenuChoice nextPage = init.run();
                 switch (nextPage) {
@@ -51,6 +69,7 @@ public class Main {
             }
 
             if (state == GameState.SAVE) {}
+            // If either Init Menu or Pause Page set state to QUit, we quit immediately
             if (state == GameState.QUIT) {
                 System.exit(0);
             }
