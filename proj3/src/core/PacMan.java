9b09e3c45a56c20b97726382551eb853e68d1ba1
GyDeen <zde421217757@outlook.com>
2025-12-07 18:30:45 +1100

    Draw image and update image based on time shift and input key

--- proj3/src/core/PacMan.java
+++ proj3/src/core/PacMan.java
@@ -1,5 +1,6 @@
 package core;
 
+import edu.princeton.cs.algs4.StdDraw;
 import tileengine.TETile;
 import tileengine.TileType;
 
@@ -22,20 +23,29 @@ public class PacMan extends GameObject{
     private String currentImage;
     private Direction curDirection;
 
+    private long nextSwitchTime = 0;
+
     public PacMan(int x, int y, int width, int height) {
         super(x, y, width, height);
         setImagePath("resources/pac man/pac man & life counter & death");
     }
 
 
-    /* Switching image path based when PacMan is alive*/
-    private void changeImageAlive() {
-        if (Objects.equals(currentImage, activeImage[0])) {
-            currentImage = activeImage[1];
-        } else {
-            currentImage = activeImage[0];
+    /** Switching image path based when PacMan is alive*/
+    public void updateImageBasedOnTime(long worldTimeMs) {
+        if (!isActive()) return;
+
+        if (worldTimeMs > nextSwitchTime) {
+            if (Objects.equals(currentImage, activeImage[0])) {
+                currentImage = activeImage[1];
+            } else {
+                currentImage = activeImage[0];
+            }
         }
     }
+
+    /** Return current facing direction */
+    public Direction getDirection() {return curDirection;}
 
 
     public void dying() {
@@ -47,34 +57,64 @@ public class PacMan extends GameObject{
         setPosition(x, y);
     }
 
-    /* Movement update based on input key */
-    private void updateBasedInput(Direction direction, TETile[][] world) {
-        Point curPosition = getPosition();
-        switch (direction) {
-            case UP:
-                if (!TileType.toType(world[curPosition.x][curPosition.y + 1]).isPassable()) return;
-                move(curPosition.x, curPosition.y + 1);
-                curDirection = Direction.UP;
-                break;
-            case DOWN:
-                if (!TileType.toType(world[curPosition.x][curPosition.y - 1]).isPassable()) return;
-                move(curPosition.x, curPosition.y - 1);
-                curDirection = Direction.DOWN;
-                break;
-            case LEFT:
-                if (!TileType.toType(world[curPosition.x - 1][curPosition.y]).isPassable()) return;
-                move(curPosition.x - 1, curPosition.y);
-                curDirection = Direction.LEFT;
-                break;
-            case RIGHT:
-                if (!TileType.toType(world[curPosition.x + 1][curPosition.y]).isPassable()) return;
-                move(curPosition.x + 1, curPosition.y);
-                curDirection = Direction.RIGHT;
-                break;
-        }
+    private void validMove(int x, int y, TETile[][] world) {
+        if (!TileType.toType(world[x][y]).isPassable())  return;
+
+        move(x, y);
     }
 
 
-    /** Return current facing direction */
-    public Direction getDirection() {return curDirection;}
+    /* Movement update based on input key */
+    private void updateBasedInput(Direction direction, TETile[][] world) {
+        if (!isActive()) return;
+
+        int newX = getPosition().x, newY = getPosition().y;
+        Point curPosition = getPosition();
+        switch (direction) {
+            case UP:
+                newX = curPosition.x; newY = curPosition.y + 1;
+                curDirection = Direction.UP;
+                break;
+            case DOWN:
+                newX = curPosition.x; newY = curPosition.y - 1;
+                curDirection = Direction.DOWN;
+                break;
+            case LEFT:
+                newX = curPosition.x - 1; newY = curPosition.y;
+                curDirection = Direction.LEFT;
+                break;
+            case RIGHT:
+                newX = curPosition.x + 1; newY = curPosition.y;
+                curDirection = Direction.RIGHT;
+                break;
+        }
+
+        validMove(newX, newY, world);
+    }
+
+
+    @Override
+    /** Draw the image based on current facing. It should be able switch the image based on the time. */
+    public void drawImage() {
+        double angle = 0d;
+        int scaledX = 1, scaledY = 1;
+
+        switch (curDirection) {
+            case UP:
+                angle = 90;
+                break;
+            case DOWN:
+                angle = -90;
+                break;
+            case LEFT:
+                angle = 0;
+                scaledX = -1;
+                break;
+            default:
+                angle = 0;
+                break;
+        }
+
+        StdDraw.picture(getPosition().x, getPosition().y, currentImage, scaledX, scaledY, angle);
+    }
 }
