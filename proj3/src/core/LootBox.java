6d88c50da4698398c0471915544c870e370249b2
GyDeen <zde421217757@outlook.com>
2025-12-09 16:09:00 +1100

    Basic implementation

--- proj3/src/core/LootBox.java
+++ proj3/src/core/LootBox.java
@@ -5,13 +5,19 @@ import tileengine.TileType;
 
 import javax.swing.*;
 
+import static core.Config.FINAL_BOX;
+import static core.Config.MYSTERY_BOX;
+
 public class LootBox extends GameObject {
-    MainRoom belongsTo;
+    private MainRoom belongsTo;
+    private int boxType;
 
     /** Loot Box take up 2 x 2 tiles. Therefore, it stores its top-left tile as the position */
-    public LootBox(MainRoom room, int x, int y, String imagePath,int width, int height) {
-        super(x, y, imagePath, width, height);
+    public LootBox(MainRoom room, int x, int y,int width, int height, int boxType) {
+        super(x, y, width, height);
         belongsTo = room;
+        if (boxType == FINAL_BOX) {setImagePath("resources/loot box/Final Box"); boxType = FINAL_BOX;}
+        if (boxType == MYSTERY_BOX) {setImagePath("resources/loot box/mystery box/vecteezy_pixel-art-illustration-gift-box-pixelated-party-gift_32858111.jpg"); boxType = MYSTERY_BOX;}
     }
 
 
@@ -24,6 +30,14 @@ public class LootBox extends GameObject {
     }
 
 
+    public void disappear(World w) {
+        if (boxType == MYSTERY_BOX) {
 
+        }
+
+        if  (boxType == FINAL_BOX) {
+            w
+        }
+    }
 }
 
