627c7529ebe3977dfecc2d092d65f28ccd51ea79
GyDeen <zde421217757@outlook.com>
2025-12-09 14:38:35 +1100

    Working on loot box

---
+++ proj3/src/core/LootBox.java
@@ -0,0 +1,29 @@
+package core;
+
+import tileengine.TETile;
+import tileengine.TileType;
+
+import javax.swing.*;
+
+public class LootBox extends GameObject {
+    MainRoom belongsTo;
+
+    /** Loot Box take up 2 x 2 tiles. Therefore, it stores its top-left tile as the position */
+    public LootBox(MainRoom room, int x, int y, String imagePath,int width, int height) {
+        super(x, y, imagePath, width, height);
+        belongsTo = room;
+    }
+
+
+    /** Check current position is valid position for the loot box */
+    public static boolean validPos(int x, int y, TETile[][] world) {
+        if (x < 0 || y < 0 || x >= world.length || y >= world[0].length) return false;
+
+        return TileType.toType(world[x][y]).isPassable() && TileType.toType(world[x + 1][y]).isPassable()
+                && TileType.toType(world[x][y - 1]).isPassable() && TileType.toType(world[x + 1][y - 1]).isPassable();
+    }
+
+
+
+}
+
