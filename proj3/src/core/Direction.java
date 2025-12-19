038bf07054ff81a50e447434e2c51833d5596b0f
GyDeen <zde421217757@outlook.com>
2025-12-19 15:10:50 +1100

    Add to string method

--- proj3/src/core/Direction.java
+++ proj3/src/core/Direction.java
@@ -2,4 +2,17 @@ package core;
 
 public enum Direction {
     LEFT, RIGHT, UP, DOWN;
+    public String toString() {
+        switch (this) {
+            case LEFT:
+                return "Left";
+            case RIGHT:
+                return "Right";
+            case UP:
+                return "Up";
+            case DOWN:
+                return "Down";
+        }
+        return "";
+    }
 }
