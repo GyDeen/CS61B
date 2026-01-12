cf60584e7c9570b48f13520f920d218354626dd2
GyDeen <zde421217757@outlook.com>
2026-01-12 15:10:38 +1100

    Split each alpha into quarters to better assign tiles for the image

--- proj3/src/core/MysteryBox.java
+++ proj3/src/core/MysteryBox.java
@@ -10,7 +10,7 @@ import java.util.Random;
 import static core.Config.*;
 
 public class MysteryBox extends LootBox {
-    private String[] fadeMysteryPath;
+    private String[][] fadeMysteryPath;
 
     private boolean fading = false;
     private int fadeStep = 0;
@@ -21,20 +21,23 @@ public class MysteryBox extends LootBox {
     private MysteryBox(MainRoom belongsTo, int x, int y, int width, int height) {
         super(belongsTo, x, y, width, height);
         setImagePath("resources/loot box/mystery box/");
-        fadeMysteryPath = new String[]{
-                getImagePath() + "gift_box_alpha_100.jpg",
-                getImagePath() + "gift_box_alpha_70.jpg",
-                getImagePath() + "gift_box_alpha_40.jpg",
-                getImagePath() + "gift_box_alpha_15.jpg",
-        };
+        fadeMysteryPath = new String[4][4];
+        String[] alphas = {"100", "70", "40", "15"};
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                // j + 2 matches your files: -2.jpg, -3.jpg, -4.jpg, -5.jpg
+                fadeMysteryPath[i][j] = getImagePath() + "gift_box_alpha_" + alphas[i] + "-" + (j + 2) + ".jpg";
+            }
+        }
     }
 
 
     public static MysteryBox generateMysteryBox(MainRoom belongsTo, TETile[][] world, Random rand) {
         Point p = findSpawnLocation(belongsTo, rand, world);
         world[p.x][p.y] = Tileset.CELL;
-        world[p.x + 1][p.y] = Tileset.CELL;
         world[p.x][p.y - 1] = Tileset.CELL;
+        world[p.x + 1][p.y] = Tileset.CELL;
         world[p.x + 1][p.y - 1] = Tileset.CELL;
         return new MysteryBox(belongsTo, p.x, p.y, 2, 2);
     }
@@ -42,7 +45,7 @@ public class MysteryBox extends LootBox {
 
     /** Handle fading */
     public int update(World world) {
-        if (!fading) {StdDraw.picture(getPosition().x + 1, getPosition().y, fadeMysteryPath[fadeStep], 2, 2); return NOT_FADING;}
+        if (!fading) {StdDraw.picture(getPosition().x + 0.5, getPosition().y + 0.5, fadeMysteryPath[fadeStep], 2, 2); return NOT_FADING;}
         if (world.getElapsedTimeMs() < nextFadeTimeMs) return FADING;
 
         if (fadeStep < fadeMysteryPath.length) {
