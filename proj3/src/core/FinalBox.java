5a6626c801974d0a95e8eb44e2c5927947f1550b
GyDeen <zde421217757@outlook.com>
2026-01-25 18:09:31 +1100

    Rendering FinalBox

--- proj3/src/core/FinalBox.java
+++ proj3/src/core/FinalBox.java
@@ -9,21 +9,34 @@ import java.util.Random;
 import static core.Config.WIN;
 
 public class FinalBox extends LootBox {
+    private String[] openFinalBox;
+    private int openStep = 0;
+    private boolean opening = false;
 
     /* Using the top left tile as the position with 2 x 2 size */
     private FinalBox(MainRoom room, int x, int y, int width, int height) {
         super(room, x, y, width, height);
         setImagePath("resources/loot box/Final Box");
+        openFinalBox = new String[4];
+        for (int i = 0; i < openFinalBox.length; i++) {
+            if (i == 0) openFinalBox[i] = getImagePath() + "RPG Chests.jpg";
+            else openFinalBox[i] = getImagePath() + "RPG Chests" + "-" + (i + 1) + ".jpg";
+        }
     }
 
 
     /** Generator for final box */
     public static FinalBox generateFinalBox(MainRoom initialRoom, Random rand, TETile[][] world) {
         Point p = findSpawnLocation(initialRoom, rand, world);
-        return new FinalBox(initialRoom, p.x, p.y, 2, 2);
+        assert p != null;
+        return new FinalBox(initialRoom, p.x, p.y, 1, 1);
     }
 
     public int update(World world) {
-        return WIN;
+        Point p = getPosition();
+
     }
+
+
+    public void startOpening() {opening = true;}
 }
