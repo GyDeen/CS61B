cf309a80bf19a7ef6f72f348728b4f85dae9a290
GyDeen <zde421217757@outlook.com>
2026-01-25 20:55:24 +1100

    Start implementing buff and debuff

---
+++ proj3/src/core/EffectDealer.java
@@ -0,0 +1,18 @@
+package core;
+
+public class EffectDealer {
+    public enum EffectType {
+        COINS,
+        SPEED_Up,
+        SLOW,
+        GHOST_STun,
+        GHOST_SpeedUp,
+        REDUCE_Time,
+        ADD_Time,
+        TIME_Froze,
+        WEAPONIZE,
+        KEY,
+        INVERTED_Move,
+        GHOST_Siren
+    }
+}
