c95ba5b93d0685d0601e1e67b8aaa71644ead733
GyDeen <zde421217757@outlook.com>
2026-02-01 20:39:57 +1100

    Get random effect

--- proj3/src/core/EffectDealer.java
+++ proj3/src/core/EffectDealer.java
@@ -1,18 +1,49 @@
 package core;
 
+import tileengine.TileType;
+
+import java.util.Random;
+
 public class EffectDealer {
     public enum EffectType {
-        COINS,
-        SPEED_Up,
-        SLOW,
-        GHOST_STun,
-        GHOST_SpeedUp,
-        REDUCE_Time,
-        ADD_Time,
-        TIME_Froze,
-        WEAPONIZE,
-        KEY,
-        INVERTED_Move,
-        GHOST_Siren
+        COINS(false),
+        SPEED_Up(false),
+        GHOST_STun(false),
+        ADD_Time(false),
+        TIME_Froze(false),
+        WEAPONIZE(false),
+        KEY(false),
+        SLOW(true),
+        GHOST_SpeedUp(true),
+        REDUCE_Time(true),
+        INVERTED_Move(true),
+        GHOST_Siren(true);
+
+
+        private final boolean isDeBuff;
+        EffectType(boolean isDeBuff) {
+            this.isDeBuff = isDeBuff;
+        }
+    }
+
+    public static EffectType getRandomEffect(Random rand) {
+        return EffectType.values()[rand.nextInt(EffectType.values().length)];
+    }
+
+    public static EffectType getRandomBuff(Random rand) {
+        return EffectType.values()[rand.nextInt(7)];
+    }
+
+
+    public static EffectType getRandomDebuff(Random rand) {
+        return EffectType.values()[rand.nextInt(7,11)];
+    }
+
+
+    public void applyEffect(EffectType effectType, World world) {
+        switch (effectType) {
+            case COINS:
+
+        }
     }
 }
