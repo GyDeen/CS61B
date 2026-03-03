4779f6aa65715366fce8e3d32fe1ace346e3c102
GyDeen <zde421217757@outlook.com>
2026-03-03 12:31:44 +1100

    Starting on generating route for ghost

--- proj3/src/core/FixRouteGhost.java
+++ proj3/src/core/FixRouteGhost.java
@@ -1,4 +1,38 @@
 package core;
 
+import tileengine.TETile;
+
+import java.awt.*;
+import java.util.Random;
+
 public class FixRouteGhost extends Ghost{
+    private static int GAURD = 1;
+    private static int ROUGUE = 2;
+
+
+    private Point[] route;
+    private int currentIdx = 0;
+
+    private FixRouteGhost(int x, int y, int width, int height, Random rand) {
+        super(x, y, width, height, rand);
+        setImagePath("resources/pac man/ghost/pink ghost");
+    }
+
+    public static FixRouteGhost generateFixRouteGhost(Random rand, TETile[][] world, GameObject closeTo) {
+        Point p = findSpawnLocation(rand.nextInt(3, 5), 1, rand, world, closeTo);
+        if (p == null) return null;
+
+    }
+
+
+    public Point[] generateRoute(Random rand, TETile[][] world) {
+        if (rand.nextInt(1, 3) == GAURD) {
+            return generateGuardRoute();
+        }
+    }
+
+
+    private Point[] generateGuardRoute() {
+
+    }
 }
