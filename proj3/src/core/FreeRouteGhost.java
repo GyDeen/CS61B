6f68ae1e492f9acf543e95f67c448ad16bb2e2dc
GyDeen <zde421217757@outlook.com>
2026-03-07 16:31:46 +1100

    Default constructor for code testing

--- proj3/src/core/FreeRouteGhost.java
+++ proj3/src/core/FreeRouteGhost.java
@@ -1,4 +1,9 @@
 package core;
 
+import java.util.Random;
+
 public class FreeRouteGhost extends Ghost{
+    protected FreeRouteGhost(int x, int y, int width, int height, Random rand) {
+        super(x, y, width, height, rand);
+    }
 }
--- proj3/src/core/LurkGhost.java
+++ proj3/src/core/LurkGhost.java
@@ -1,4 +1,11 @@
 package core;
 
+import java.util.Random;
+
 public class LurkGhost extends Ghost {
+
+
+    protected LurkGhost(int x, int y, int width, int height, Random rand) {
+        super(x, y, width, height, rand);
+    }
 }
